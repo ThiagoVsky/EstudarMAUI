773c5a0400070c641545e096a04a4432e299e104
Thiago Moraes <thiago.moraes582@gmail.com>
2024-08-21 22:07:58 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EstudarMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudarMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
